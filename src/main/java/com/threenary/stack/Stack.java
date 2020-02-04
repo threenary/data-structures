7e7da51fd5bdbe8f0d40607f2cd5469ab3ebcd41
Gonzalo Gomez Sullain <gonzalo.gomez@codurance.com>
2020-02-04 10:31:47 +0100

    :wrench: WIP on the Stack implementation

--- src/main/java/com/threenary/stack/Stack.java
+++ src/main/java/com/threenary/stack/Stack.java
@@ -1,8 +1,27 @@
 package com.threenary.stack;
 
-/**
- * TBD
- */
-public class Stack {
 
+import com.threenary.node.Node;
+
+/**
+ * Self implementation of a Stack
+ *
+ * @param <T>
+ */
+public class Stack<T> {
+
+    private Node<T> head;
+
+    public boolean isEmpty() {
+        return (head == null);
+    }
+
+    public T peek() {
+        return isEmpty() ? null : head.getData();
+    }
+
+    public void push(Node<T> node) {
+        node.setNext(head);
+        this.head = node;
+    }
 }
--- src/test/java/com/threenary/stack/StackShould.java
+++ src/test/java/com/threenary/stack/StackShould.java
@@ -1,5 +1,44 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.threenary.stack;
+
+import com.threenary.node.Node;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StackShould {
+
+    private Stack<String> stack;
+
+    @BeforeEach
+    public void setUp(){
+        stack = new Stack<>();
+    }
+
+    @Test
+    public void return_true_if_stack_has_no_elements() {
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void adds_element_to_the_stack() {
+        Node<String> node = new Node<>("element");
+        stack.push(node);
+
+        assertFalse(stack.isEmpty());
+    }
+
+
+    @Test
+    public void return_the_head_data_when_no_empty() {
+        String element = "element";
+        Node<String> node = new Node<>(element);
+        stack.push(node);
+
+        assertThat(stack.peek()).isEqualTo(element);
+    }
+
 
 }
